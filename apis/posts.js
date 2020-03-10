92781e07c65b1d9a82a851dc1082227e678de3a7
Zak <zakadmaster@gmail.com>
2020-03-10 14:30:04 +0100

    Addition of post

--- apis/posts.js
+++ apis/posts.js
@@ -17,13 +17,23 @@ router.get("/api/posts/:id", (req, res) => {
     const {id} = req.params;
 
     helpers.findById(id)
-    .then(user => {
-        res.status(200).json(user)
+    .then(post => {
+        res.status(200).json(post)
     })
     .catch(err => {
-        res.status(500).json({ errorMessage: "error "})
+        res.status(500).json({ errorMessage: "error" })
     })
 })
 
+router.post("/api/posts/", (req, res) => {
+    const payload = req.body;
+    helpers.insert(payload)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "error" })
+    })
+})  
 
 module.exports = router
